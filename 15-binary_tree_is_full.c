7900615ce7eeab0d5c2d6edd931f8783356f15f3
zyclamenace <gemicieneshakki@gmail.com>
2024-08-06 16:10:06 +0200

    task 15 finished

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,20 @@
 #include "binary_trees.h"
 /**
- *  - 
- * 
+ * binary_tree_is_full - tree is full is node got 2 child or 0
+ * @tree: pointer to root of the tree
  *
- * Return:
+ * Return: 0 tree NULL or not full, 1 tree full
  */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
