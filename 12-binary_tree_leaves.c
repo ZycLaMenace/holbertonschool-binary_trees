df47220d63b59221d79ab50eab4dbe19b1c7a888
sifir-gun <guney.tasdelen74@gmail.com>
2024-08-06 15:34:56 +0200

    add tsk 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,26 @@
 #include "binary_trees.h"
 /**
- *  - 
- * 
+ * binary_tree_leaves - Counts the leaves in a binary tree
  *
- * Return:
+ * @tree: Pointer to the root node of the tree to count the number of leaves
+ * Description: Counts the leaves in a binary tree
+ * A NULL pointer is not a leaf
+ *
+ * Return: Number of leaves in the tree, or 0 if tree is NULL
  */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+	return (leaves);
+}
