32c86dc924fe54d6f4c3180fe46476b32fd33d7b
zyclamenace <gemicieneshakki@gmail.com>
2024-08-08 09:37:37 +0200

    adding the function 9 directy on the file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree to measure the balance factor
@@ -15,4 +14,25 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int right_height = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
 
 	return (left_height - right_height);
+}
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
